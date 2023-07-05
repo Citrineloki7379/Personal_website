336e62e6d829390df9fabb9e8858fc098a0a405e
Manav Sankriti <citrineloki7379@gmail.com>
2023-07-05 16:25:48 +0100

    Implement index so model canvases can be exported

---
+++ src/components/canvas/index.js
@@ -0,0 +1,5 @@
+import ShipCanvas from "./Ship";
+import ParametricSphereCanvas from "./ParametricSphere";
+import StarsCanvas from "./Stars";
+
+export { ShipCanvas, ParametricSphereCanvas, StarsCanvas };
