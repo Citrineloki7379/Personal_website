ba2c8a4b1a95722966032308af2d0c5df53bbae3
Manav Sankriti <citrineloki7379@gmail.com>
2023-07-05 15:46:22 +0100

    Add Assets

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import menu from './menuBlue.png';
+import close from './cross.png';
+import imperial from './imperial.png';
+import westhill from './westhill.jpg';
+import github from './github.png';
+import pwebsite from './personal_website.png';
+import game2048 from './2048.png';
+import dash from './dash.png';
+
+export {
+    logo,
+    menu,
+    close,
+    imperial,
+    westhill,
+    github,
+    pwebsite,
+    game2048,
+    dash
+};
