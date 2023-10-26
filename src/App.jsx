c3eba6b7a2e9fe5a1a92083fa081052a1917f0c8
Manav Sankriti <ms3822@ic.ac.uk>
2023-10-26 14:43:16 +0100

    Implement window resize handler

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,51 @@ import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 import { useEffect, useState, useRef } from 'react';
-import TOPOLOGY from 'vanta/dist/vanta.topology.min'
-
+import TOPOLOGY from 'vanta/src/vanta.topology'
+// import TOPOLOGY from 'vanta/dist/vanta.topology.min.js'
 const App = () => {
 
   const [vantaEffect, setVantaEffect] = useState(0);
   const vantaRef = useRef(null);
 
+  const[dims, setDims] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
+
   useEffect(() => {
-    if (!vantaEffect) {
+    window.addEventListener('unload', function () {
+      document.documentElement.innerHTML = '';
+    });
+
+    return () => {
+      window.removeEventListener('unload', function () {
+        document.documentElement.innerHTML = '';
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setDims({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+
+  }, []);
+
+  useEffect(() => {
+    // console.log("Width " + dims.width);
+    // console.log("Height " + dims.height);
+    // console.log("Vanta Effect " + vantaEffect);
+
+    if(/*.height >= 250 && dims.width >= 250 && */!vantaEffect) {
       setVantaEffect(
         TOPOLOGY({
           el: vantaRef.current,
@@ -28,18 +64,49 @@ const App = () => {
         }) 
       );
     }
+
     return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, [vantaEffect]);
+      if(vantaEffect) {
+        vantaEffect.destroy();
+        setVantaEffect(null);
+
+      }
+    }
+  }, [dims.height, dims.width]);
+
+  // useEffect(() => {
+  //   if (!vantaEffect) {
+  //     setVantaEffect(
+  //       TOPOLOGY({
+  //         el: vantaRef.current,
+  //         mouseControls: true,
+  //         touchControls: true,
+  //         gyroControls: false,
+  //         forceAnimate: true,
+  //         minHeight: 200.00,
+  //         minWidth: 200.00,
+  //         scale: 1.0,
+  //         scaleMobile: 1.0,
+  //         color: 0x8499e3,
+  //         backgroundColor: 0x1011b
+  //       }) 
+  //     );
+  //   }
+  //   return () => {
+  //     if (vantaEffect) vantaEffect.destroy();
+  //   };
+  // }, [vantaEffect]);
 
   return (
     <div ref={vantaRef} className="bg z-0" id="vanta">
     <BrowserRouter> 
         <div className="relative z-5">
           <Navbar />
-          <Hero />
-          <About />
+          <div className="relative z-5">
+            <Hero />
+            <About />
+            <StarsCanvas />
+          </div>
           <Experience />
           {/* <Tech /> */}
           <Works />
