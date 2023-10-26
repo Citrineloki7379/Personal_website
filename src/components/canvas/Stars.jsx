e62f0d3aaad2509b44ae0537327825ba853d02df
Manav Sankriti <ms3822@ic.ac.uk>
2023-10-26 14:42:22 +0100

    Fix position of particles on stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,7 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(1000), { radius: 1.1 }));
+  const [sphere] = useState(() => random.inSphere(new Float32Array(1200), { radius: 1.1 }));
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -14,7 +14,7 @@ const Stars = (props) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
         <PointMaterial
           transparent
           color='#f272c8'
