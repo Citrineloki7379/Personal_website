45c2782b335d11f01010e17dedb0cdde55c26f7c
Manav Sankriti <ms3822@ic.ac.uk>
2023-10-28 17:43:26 +0100

    Alter speed of rotation for mobile

--- src/components/canvas/ParametricSphere.jsx
+++ src/components/canvas/ParametricSphere.jsx
@@ -55,7 +55,7 @@ const ParametricSphereCanvas = () => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
         autoRotate={true}
-        autoRotateSpeed={2}/>
+        autoRotateSpeed={isMobile ? 1.25 : 2}/>
 
         <ParametricSphere isMobile={isMobile}/>
       </Suspense>
